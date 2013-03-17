18f807d1dc36779fb2b00cb1ff4fa7127b216d15
Kamil Stasiak <kamusissa@gmail.com>
2013-03-18 00:46:20 +0100

    Add test for help page

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,10 +1,22 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
-      visit '/static pages/home'
-      page.should have content('Sample App')
+      visit '/static_pages/home'
+#      page.should have content('Sample App')
+      expect(page).to have_text("Sample App")
     end
   end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+#      page.should have content('Help')
+      expect(page).to have_text("Help")
+    end
+  end
+
+
 end
