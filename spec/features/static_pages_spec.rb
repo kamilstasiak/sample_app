e88c0420d3bffd864affe6d58ca8a637620d9d7e
Kamil Stasiak <kamusissa@gmail.com>
2013-03-19 23:42:00 +0100

    Add tests for static pages titles

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -5,25 +5,45 @@ describe "Static pages" do
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-#      page.should have content('Sample App')
-      expect(page).to have_text("Sample App")
+#      expect(page).to have_text("Sample App")
+      expect(page).to have_selector('h1', :text => "Sample App")
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+#      expect(page).to have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+      expect(first('title').native.text).to eq "Ruby on Rails Tutorial Sample App | Home"
     end
   end
 
   describe "Help page" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
-#      page.should have content('Help')
-      expect(page).to have_text("Help")
+#      expect(page).to have_text("Help")
+      expect(page).to have_selector('h1', :text => "Help")
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+#      expect(page).to have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+      expect(first('title').native.text).to eq "Ruby on Rails Tutorial Sample App | Help"
     end
   end
 
   describe "About page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_text("About Us")
+#      expect(page).to have_text("About Us")
+      expect(page).to have_selector('h1', :text => "About Us")
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+#      expect(page).to have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About")
+      expect(first('title').native.text).to eq "Ruby on Rails Tutorial Sample App | About Us"
     end
   end
+
 
 
 end
