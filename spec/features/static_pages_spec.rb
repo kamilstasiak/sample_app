3ef7241ce5da3c88332d06736b0df4a561f9cc17
Kamil Stasiak <kamusissa@gmail.com>
2013-03-31 21:11:30 +0200

    Refactor static_pages tests, add full_title helper method

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -3,61 +3,50 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Home page" do
+    before { visit root_path }
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-#      expect(page).to have_text("Sample App")
       expect(page).to have_selector('h1', :text => "Sample App")
     end
 
     it "should have the right title" do
-      visit '/static_pages/home'
-#      expect(page).to have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
-      expect(first('title').native.text).to eq "Ruby on Rails Tutorial Sample App"
+      expect(first('title').native.text).to eq full_title('')
     end
 
     it "should not have a custom page title" do
-      visit "/static_pages/home"
       expect(first('title').native.text).not_to include "| Home"
     end
   end
 
   describe "Help page" do
+    before { visit help_path }
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
-#      expect(page).to have_text("Help")
       expect(page).to have_selector('h1', :text => "Help")
     end
 
     it "should have the right title" do
-      visit '/static_pages/help'
-#      expect(page).to have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
-      expect(first('title').native.text).to eq "Ruby on Rails Tutorial Sample App | Help"
+      expect(first('title').native.text).to eq full_title("Help")
     end
   end
 
   describe "About page" do
+    before { visit about_path }
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-#      expect(page).to have_text("About Us")
       expect(page).to have_selector('h1', :text => "About Us")
     end
 
     it "should have the right title" do
-      visit '/static_pages/about'
-#      expect(page).to have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About")
-      expect(first('title').native.text).to eq "Ruby on Rails Tutorial Sample App | About Us"
+      expect(first('title').native.text).to eq full_title("About Us")
     end
   end
 
   describe "Contact page" do
+    before { visit contact_path }
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
       expect(page).to have_selector('h1', :text => "Contact")
     end
 
     it "should have the right title" do
-      visit '/static_pages/contact'
-      expect(first('title').native.text).to eq "Ruby on Rails Tutorial Sample App | Contact"
+      expect(first('title').native.text).to eq full_title("Contact")
     end
   end
 
